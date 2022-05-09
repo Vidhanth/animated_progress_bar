3a7ec2b5d62ed283a5d593eaca5aa3effcbe518c
Vidhanth <vidhanthbafna@gmail.com>
2022-05-09 22:07:05 +0530

    Minor changes: * By default expand progress bar to max width * Animate the progress when mounted, allow custom delay

--- lib/animated_progress_bar.dart
+++ lib/animated_progress_bar.dart
@@ -2,8 +2,8 @@ library animated_progress_bar;
 
 import 'package:flutter/material.dart';
 
-class AnimatedProgressBar extends StatelessWidget {
-  final double width;
+class AnimatedProgressBar extends StatefulWidget {
+  final double? width;
   final double height;
   final BorderRadius radius;
   final double value;
@@ -12,11 +12,12 @@ class AnimatedProgressBar extends StatelessWidget {
   final Duration duration;
   final Curve curve;
   final bool centered;
+  final Duration delay;
 
   const AnimatedProgressBar({
     Key? key,
-    this.width = 200,
-    this.height = 2,
+    this.width,
+    this.height = 3,
     this.radius = BorderRadius.zero,
     this.bgColor = Colors.white54,
     this.progressColor = Colors.white,
@@ -24,29 +25,56 @@ class AnimatedProgressBar extends StatelessWidget {
     this.centered = false,
     required this.value,
     this.curve = Curves.fastOutSlowIn,
-  }) : assert(value >= 0 && value <= 1), super(key: key);
+    this.delay = Duration.zero,
+  })  : assert(value >= 0 && value <= 1),
+        super(key: key);
+
+  @override
+  State<AnimatedProgressBar> createState() => _AnimatedProgressBarState();
+}
+
+class _AnimatedProgressBarState extends State<AnimatedProgressBar> {
+  bool animate = false;
+
+  @override
+  void initState() {
+    if (!animate) {
+      Future.delayed(widget.delay, () {
+        setState(() {
+          animate = true;
+        });
+      });
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: radius,
-      child: Stack(
-        alignment: centered ? Alignment.center : Alignment.centerLeft,
-        children: <Widget>[
-          Container(
-            width: width,
-            height: height,
-            color: bgColor,
-          ),
-          AnimatedContainer(
-            width: value * width,
-            height: height,
-            color: progressColor,
-            curve: curve,
-            duration: duration,
-          ),
-        ],
-      ),
+      borderRadius: widget.radius,
+      child: LayoutBuilder(builder: (context, constraints) {
+        return Stack(
+          alignment: widget.centered ? Alignment.center : Alignment.centerLeft,
+          children: <Widget>[
+            Container(
+              width: widget.width ?? constraints.maxWidth,
+              height: widget.height,
+              color: widget.bgColor,
+            ),
+            ClipRRect(
+                borderRadius: widget.radius,
+                child: AnimatedContainer(
+                  width: animate
+                      ? widget.value * (widget.width ?? constraints.maxWidth)
+                      : 0,
+                  height: widget.height,
+                  color: widget.progressColor,
+                  curve: widget.curve,
+                  duration: widget.duration,
+                )),
+          ],
+        );
+      }),
     );
   }
 }
